8a88c73187566eb3a65d2897aba56bfff3911195
LNUitTutor <lnu.it.tutor@gmail.com>
2023-05-22 09:32:39 +0300

    Add project files.

---
+++ WorkShopLast/MainForm.Designer.cs
@@ -0,0 +1,144 @@
+﻿
+namespace WorkShopLast
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonBreak = new System.Windows.Forms.Button();
+            this.buttonQuit = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(15, 66);
+            this.progressBar.Margin = new System.Windows.Forms.Padding(4);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(864, 29);
+            this.progressBar.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(15, 29);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(262, 20);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Хід обчислень (від 0 до 100%)";
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(124, 122);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(130, 37);
+            this.buttonRun.TabIndex = 2;
+            this.buttonRun.Text = "Почати";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            this.buttonRun.Click += new System.EventHandler(this.buttonRun_Click);
+            // 
+            // buttonBreak
+            // 
+            this.buttonBreak.Location = new System.Drawing.Point(297, 122);
+            this.buttonBreak.Name = "buttonBreak";
+            this.buttonBreak.Size = new System.Drawing.Size(130, 37);
+            this.buttonBreak.TabIndex = 3;
+            this.buttonBreak.Text = "Перервати";
+            this.buttonBreak.UseVisualStyleBackColor = true;
+            this.buttonBreak.Click += new System.EventHandler(this.buttonBreak_Click);
+            // 
+            // buttonQuit
+            // 
+            this.buttonQuit.Location = new System.Drawing.Point(631, 122);
+            this.buttonQuit.Name = "buttonQuit";
+            this.buttonQuit.Size = new System.Drawing.Size(130, 37);
+            this.buttonQuit.TabIndex = 4;
+            this.buttonQuit.Text = "Завершити";
+            this.buttonQuit.UseVisualStyleBackColor = true;
+            this.buttonQuit.Click += new System.EventHandler(this.buttonQuit_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(422, 29);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "0%";
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Enabled = false;
+            this.buttonClear.Location = new System.Drawing.Point(466, 122);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(130, 37);
+            this.buttonClear.TabIndex = 6;
+            this.buttonClear.Text = "Очистити";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(894, 197);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.buttonQuit);
+            this.Controls.Add(this.buttonBreak);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.progressBar);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Тривалі обчислення";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ProgressBar progressBar;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonBreak;
+        private System.Windows.Forms.Button buttonQuit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonClear;
+    }
+}
+
---
+++ WorkShopLast/MainForm.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WorkShopLast
+{
+    public partial class MainForm : Form
+    {
+        CancellationTokenSource source;
+        IProgress<(string, int)> progress;
+        public MainForm()
+        {
+            InitializeComponent();
+            progress = new Progress<(string, int)>(
+                value => {
+                    label2.Text = value.Item1;
+                    progressBar.Value = value.Item2;
+                });
+        }
+
+        private void buttonQuit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private async void buttonRun_Click(object sender, EventArgs e)
+        {
+            buttonClear.Enabled = true;
+            buttonRun.Enabled = false;
+
+            source = new CancellationTokenSource();
+            CancellationToken token = source.Token;
+            
+            int result = await Task.Run(() =>
+            {
+                int i = 0;
+                for (; i <= 100; ++i)
+                {
+                    progress.Report((i.ToString() + "%", i));
+                    Thread.Sleep(200);
+                    if (i % 20 == 0 && token.IsCancellationRequested) break;
+                }
+                return i;
+            });
+            MessageBox.Show($" The value returned by Thread is {result}", "Result");
+        }
+
+        private void buttonBreak_Click(object sender, EventArgs e)
+        {
+            //progressBar.Value -= 5;
+            source.Cancel();
+        }
+
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            buttonClear.Enabled = false;
+            buttonRun.Enabled = true;
+
+            label2.Text = "0%";
+            progressBar.Value = 0;
+        }
+    }
+}
